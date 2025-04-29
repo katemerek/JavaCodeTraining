f770791cb99b7418861ccb162cb3b2d01f877d96
Ekaterina Merkulova <kate_merek@mail.ru>
2025-04-29 14:44:07 +0300

    1

---
+++ src/main/java/com/github/katemerek/javacodetraining/JavaCodeTrainingApplication.java
@@ -0,0 +1,12 @@
+package com.github.katemerek.javacodetraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCodeTrainingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaCodeTrainingApplication.class, args);
+    }
+}
---
+++ src/test/java/com/github/katemerek/javacodetraining/JavaCodeTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.katemerek.javacodetraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCodeTrainingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
